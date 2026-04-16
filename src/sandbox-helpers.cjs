42289080dc17008ac21ea6def82c75ece4e08847
CristobalUll <alu0101755601@ull.edu.es>
2026-04-16 17:59:51 +0000

    Práctica terminada

--- src/sandbox-helpers.cjs
+++ src/sandbox-helpers.cjs
@@ -36,7 +36,7 @@ function escapeRegExp(text) {
 function extractLocationFromStack(stackText, filename) {
     if (!stackText) return null;
     const escaped = escapeRegExp(filename);
-    const match = stackText.match(new RegExp(`${escaped}:(\\d+)(?::(\\d+))`)); // Second par is not optional!
+    const match = stackText.match(new RegExp(`${escaped}:(\\d+)(?::(\\d+))?`)); // Second par is not optional!
     if (!match) return null;
     return {
         line: parseInt(match[1], 10),
